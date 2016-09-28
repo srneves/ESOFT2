bc50ab656a5428211001cb94ddb7c0bb373961e1
neves <puto.neves@gmail.com>
2016-09-28 16:16:04 +0100

    add GitActivity to project

---
+++ app/src/main/java/com/example/neves/helloworld/GitActivity.java
@@ -0,0 +1,13 @@
+package com.example.neves.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GitActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_git);
+    }
+}
